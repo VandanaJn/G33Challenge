9bd53e8a965a1f491b2bd8ebd04ec627e36276aa
Vandana Jain <vandanaj@hotmail.com>
2017-08-14 23:16:44 -0400

    Adding g33_round and used it

--- FormatAmount.py
+++ FormatAmount.py
@@ -1,18 +1,26 @@
+import math
 """FormatAmount module has functions to format amounts"""
 def format_ai(val):
     """format_ai returns truncated to three decimal places, and rounded to two decimal places"""
-    return "{0:.2f}".format(trunc_decimals(val, 3))
+    return g33_round(trunc_decimals(val, 3), 2)
 
 def format_dp(val):
     """format_dp returns truncated to three decimal places"""
-    return "{0:.3f}".format(trunc_decimals(val, 3))
+    return trunc_decimals(val, 3)
 
 def format_yld(val):
     """format_yld returns truncated to four decimal places, and rounded to three decimal places"""
-    temp = trunc_decimals(val, 4)
-    return round(temp, 3)
+    return g33_round(trunc_decimals(val, 4), 3)
 
 def trunc_decimals(val, no_of_digits):
-    """trunc_decimals trucates val to no_of_digits after decimal and returns it"""
+    """trunc_decimals returns trucates val to no_of_digits after decimal"""
     trunc_plus_1 = "{0:.{1}f}".format(val, no_of_digits+1)
     return float(trunc_plus_1[:-1])
+
+def g33_round(number , no_of_digits):
+    """g33_round returns rounded no to no_of_digits decimal places, g33_round(4.65 , 1) returns 4.7 and g33_round(4.55 , 1) returns 4.6 unlike built-in round which returns banking round"""
+    if("{0:.{1}f}".format(number, no_of_digits+1)).endswith('5'):
+        return math.ceil(number * 10 ** no_of_digits) / (10 ** no_of_digits)
+    return round(number, no_of_digits)
+
+    
--- TestFormat.py
+++ TestFormat.py
@@ -1,13 +1,12 @@
 """Test Class"""
 import unittest
 
-from FormatAmount import trunc_decimals, format_ai, format_dp, format_yld
+from FormatAmount import trunc_decimals, format_ai, format_dp, format_yld, g33_round
 
 class TestFormatAmount(unittest.TestCase):
     """TestFormatAmount tests FormatAmount module"""
-    
     def test_trunc_decimals(self):
-        """test_trunc_decimals"""
+        """test_trunc_decimals - returns trucates val to no_of_digits after decimal"""
         self.assertEqual(5.67890, trunc_decimals(5.6789, 5))
         self.assertEqual(5.6789, trunc_decimals(5.6789, 4))
         self.assertEqual(5.6785, trunc_decimals(5.6785, 4))
@@ -19,38 +18,55 @@ class TestFormatAmount(unittest.TestCase):
         self.assertEqual(5555676.678, trunc_decimals(5555676.6781, 3))
 
     def test_format_ai(self):
-        """test_format_ai"""
-        self.assertEqual('5.60', format_ai(5.6))
-        self.assertEqual('5.67', format_ai(5.67))
-        self.assertEqual('5.68', format_ai(5.6789))
-        self.assertEqual('5.68', format_ai(5.6785))
-        self.assertEqual('5.67', format_ai(5.6755))## todo seems wrong
-        self.assertEqual('5.67', format_ai(5.6749))
-        self.assertEqual('5.67', format_ai(5.6744))
-        self.assertEqual('5555676.68', format_ai(5555676.6781))
+        """test_format_ai - returns truncated to three decimal places,
+        and rounded to two decimal place"""
+        self.assertEqual(5.60, format_ai(5.6))
+        self.assertEqual(5.67, format_ai(5.67))
+        self.assertEqual(5.68, format_ai(5.6789))
+        self.assertEqual(5.68, format_ai(5.6785))
+        self.assertEqual(5.68, format_ai(5.6755))
+        self.assertEqual(5.67, format_ai(5.6749))
+        self.assertEqual(5.67, format_ai(5.6744))
+        self.assertEqual(5555676.68, format_ai(5555676.6781))
 
     def test_format_dp(self):
-        """test_format_dp"""
-        self.assertEqual('5.600', format_dp(5.6))
-        self.assertEqual('5.670', format_dp(5.67))
-        self.assertEqual('5.678', format_dp(5.6789))
-        self.assertEqual('5.678', format_dp(5.6785))
-        self.assertEqual('5.675', format_dp(5.6755))
-        self.assertEqual('5.674', format_dp(5.6749))
-        self.assertEqual('5.674', format_dp(5.6744))
-        self.assertEqual('5555676.678', format_dp(5555676.6781))
+        """test_format_dp -  returns truncated to three decimal places"""
+        self.assertEqual(5.600, format_dp(5.6))
+        self.assertEqual(5.670, format_dp(5.67))
+        self.assertEqual(5.678, format_dp(5.6789))
+        self.assertEqual(5.678, format_dp(5.6785))
+        self.assertEqual(5.675, format_dp(5.6755))
+        self.assertEqual(5.674, format_dp(5.6749))
+        self.assertEqual(5.674, format_dp(5.6744))
+        self.assertEqual(5555676.678, format_dp(5555676.6781))
 
     def test_format_yld(self):
-        """test_format_yld"""
+        """test_format_yld returns truncated to four decimal places,
+        and rounded to three decimal places"""
         self.assertEqual(5.600, format_yld(5.6))
         self.assertEqual(5.670, format_yld(5.67))
         self.assertEqual(5.679, format_yld(5.6789))
         self.assertEqual(5.679, format_yld(5.67894))
-        self.assertEqual(5.678, format_yld(5.6785))## todo seems wrong
-        self.assertEqual(5.676, format_yld(5.6755))## todo investigate
+        self.assertEqual(5.679, format_yld(5.6785))
+        self.assertEqual(5.676, format_yld(5.6755))
         self.assertEqual(5.675, format_yld(5.6749))
         self.assertEqual(5.674, format_yld(5.6744))
         self.assertEqual(5555676.678, format_yld(5555676.6781))
 
-if __name__ == '__main__':
-    unittest.main(exit=False)
+    def test_g33_round(self):
+        """test_g33_round - returns rounded no to no_of_digits decimal places,
+        g33_round(4.65 , 1) returns 4.7 and g33_round(4.55 , 1) returns 4.6"""
+        self.assertEqual(5.6, g33_round(5.6, 1))
+        self.assertEqual(5.6, g33_round(5.64, 1))
+        self.assertEqual(5.7, g33_round(5.65, 1))
+        self.assertEqual(5.7, g33_round(5.66, 1))
+        self.assertEqual(5.65, g33_round(5.65, 2))
+        self.assertEqual(5.65, g33_round(5.651, 2))
+        self.assertEqual(5.65, g33_round(5.654, 2))
+        self.assertEqual(5.66, g33_round(5.655, 2))
+        self.assertEqual(5.68, g33_round(5.675, 2))
+        self.assertEqual(4.7, g33_round(4.65, 1))
+        self.assertEqual(4.6, g33_round(4.55, 1))
+
+    if __name__ == '__main__':
+        unittest.main(exit=False)
